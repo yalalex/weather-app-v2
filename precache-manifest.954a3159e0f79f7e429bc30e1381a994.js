b0a4343e528df99fa68b950579e9c13f10c11240
AlexY <alexyo87@yahoo.com>
2019-10-21 17:40:50 +0300

    Updates

---
+++ precache-manifest.954a3159e0f79f7e429bc30e1381a994.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ac139c4dd7a26087314c09bfc4196b",
+    "url": "/weather-app-redux/index.html"
+  },
+  {
+    "revision": "1d1e4c0de0643cc000f4",
+    "url": "/weather-app-redux/static/css/main.4d06d48e.chunk.css"
+  },
+  {
+    "revision": "6a70210062a6512bb4f2",
+    "url": "/weather-app-redux/static/js/2.b106c89a.chunk.js"
+  },
+  {
+    "revision": "1d1e4c0de0643cc000f4",
+    "url": "/weather-app-redux/static/js/main.20caec95.chunk.js"
+  },
+  {
+    "revision": "327a4018af741d2d9a5a",
+    "url": "/weather-app-redux/static/js/runtime~main.ca20a88f.js"
+  },
+  {
+    "revision": "706c7930509663ca9b9fcc3455d4602d",
+    "url": "/weather-app-redux/static/media/spinner.706c7930.gif"
+  }
+]);
