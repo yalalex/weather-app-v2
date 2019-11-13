c6e5ce9ee53fb306211fb7dd77506190c4e1f6d8
AlexY <alexyo87@yahoo.com>
2019-11-13 22:37:44 +0300

    minor changes

--- src/components/weather/PeriodItem.js
+++ src/components/weather/PeriodItem.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Moment from 'react-moment';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from "react";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 const PeriodItem = ({ weather: { current, lang }, period }) => {
   const { dt, main, weather } = period,
     time = dt + current.offset;
   return (
-      <div className='weatheritem'>
+    <div className='weatheritem'>
       <div className='all-center'>
         <h3>
           <Moment locale={lang} unix format='ddd LT'>
@@ -19,7 +19,7 @@ const PeriodItem = ({ weather: { current, lang }, period }) => {
         <img
           alt={weather[0].description}
           src={`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
-          style={{ width: '100px' }}
+          style={{ width: "100px" }}
         />
       </div>
       <div className='all-center'>
@@ -38,7 +38,4 @@ const mapStateToProps = state => ({
   weather: state.weather
 });
 
-export default connect(
-  mapStateToProps,
-  null
-)(PeriodItem);
+export default connect(mapStateToProps, null)(PeriodItem);
