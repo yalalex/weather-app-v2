543ec6c045aab4a3b5e9b3ec6df408722da35f2b
AlexY <alexyo87@yahoo.com>
2019-11-14 00:41:39 +0300

    Updates

--- precache-manifest.157530ee1f04226e95a1feeee31910a5.js
+++ precache-manifest.157530ee1f04226e95a1feeee31910a5.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ad04f8d972b8ae3e48d488b70072129",
+    "revision": "7b9ef6b43285cb3951a233d33272bc37",
     "url": "/weather-app-redux/index.html"
   },
   {
-    "revision": "9925a6072b5843a5a821",
-    "url": "/weather-app-redux/static/css/main.1e6eec3f.chunk.css"
+    "revision": "6be7d4c3376fcb7d6249",
+    "url": "/weather-app-redux/static/css/main.4b36f956.chunk.css"
   },
   {
     "revision": "af28fe06b002b36fcc80",
     "url": "/weather-app-redux/static/js/2.0fe84bd4.chunk.js"
   },
   {
-    "revision": "9925a6072b5843a5a821",
+    "revision": "6be7d4c3376fcb7d6249",
     "url": "/weather-app-redux/static/js/main.17909677.chunk.js"
   },
   {
